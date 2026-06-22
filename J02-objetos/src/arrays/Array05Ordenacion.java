d82d1e0e8a2d4da5b2f1b9966c3426fd905927ea
pablo <cursos.tic.plg@gmail.com>
2026-06-22 18:46:41 +0200

    ordenacion ok

--- J02-objetos/src/arrays/Array05Ordenacion.java
+++ J02-objetos/src/arrays/Array05Ordenacion.java
@@ -1,5 +1,6 @@
 package arrays;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Array05Ordenacion {
@@ -16,6 +17,27 @@ public class Array05Ordenacion {
 		}
 	}
 	
+	public static void quickSort(double[] v) {
+		quickSort(v, 0, v.length - 1);
+	}
+	
+	public static void quickSort(double[] v, int izq, int der) {
+		double valPivote = (v[izq] + v[der]) / 2;
+		int i = izq;
+		int d = der;
+		while(i < d) {
+			while(v[i] < valPivote) i++;
+			while(v[d] > valPivote) d--;
+			if(i < d) {
+				intercambia(v, i, d);
+				i++;
+				d--;
+			}
+		}
+		if (izq < d) quickSort(v, izq, d);
+		if (i < der) quickSort(v, i, der);
+	}
+	
 	public static void intercambia(double[] v, int i, int d) {
 		double aux = v[i];
 		v[i] = v[d];
@@ -24,9 +46,33 @@ public class Array05Ordenacion {
 	
 	public static void main(String[] args) {
 		double[] v1 = {77, 4, 78, 60, 100, 35, 74};
+//		insercion(v1);
+		quickSort(v1);
+		ArrayUtil.muestra(v1);
+		
+		v1 = generaArray(50);
 		insercion(v1);
 		ArrayUtil.muestra(v1);
 		
+		long t0, tf;
+		double[] v2 = generaArray(200_000_000);
+		double[] v3 = v2.clone();
+		double[] v4 = v2.clone();
+		
+//		t0 = System.currentTimeMillis();
+//		insercion(v2);
+//		tf = System.currentTimeMillis();
+//		System.out.println("Insercion: " + (tf-t0));
+		
+		t0 = System.currentTimeMillis();
+		Arrays.sort(v3);
+		tf = System.currentTimeMillis();
+		System.out.println("QuickSort libreria: " + (tf-t0));
+		
+		t0 = System.currentTimeMillis();
+		quickSort(v4);
+		tf = System.currentTimeMillis();
+		System.out.println("QuickSort: " + (tf-t0));
 	}
 	
 	public static double[] generaArray(int cant) {
