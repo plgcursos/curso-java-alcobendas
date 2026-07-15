0ed26480ea7826d5bdec3d6fc99506927bb7e693
pablo <cursos.tic.plg@gmail.com>
2026-07-15 15:54:09 +0200

    act j18

---
+++ J18-jdbc-maven/src/main/java/es/cursoalcobendas/jdbcmaven/persistencia/FabricanteDaoImpl.java
@@ -0,0 +1,15 @@
+package es.cursoalcobendas.jdbcmaven.persistencia;
+
+import java.util.List;
+
+import es.cursoalcobendas.jdbcmaven.modelo.Fabricante;
+
+public class FabricanteDaoImpl implements FabricanteDao {
+
+	@Override
+	public List<Fabricante> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
