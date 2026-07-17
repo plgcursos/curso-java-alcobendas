9b460b038a8ae517108bc98d6687f3104115f13e
plopezgrieco <plopezgrieco@gmail.com>
2026-07-17 16:12:24 +0200

    Domicilio

---
+++ Modulo-III/jpa/Domicilio.java
@@ -0,0 +1,102 @@
+package ejemplo04_insertable;
+
+import java.io.Serializable;
+
+public class Domicilio implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String tipoVia;
+	private String via;
+	private int numero;
+	private int piso;
+	private String puerta;
+	private String codigoPostal;
+	private String ciudad;
+	private String provincia;
+	
+	public Domicilio() {}
+
+	public Domicilio(String tipoVia, String via, int numero, int piso, String puerta, String codigoPostal,
+			String ciudad, String provincia) {
+		super();
+		this.tipoVia = tipoVia;
+		this.via = via;
+		this.numero = numero;
+		this.piso = piso;
+		this.puerta = puerta;
+		this.codigoPostal = codigoPostal;
+		this.ciudad = ciudad;
+		this.provincia = provincia;
+	}
+
+	public String getTipoVia() {
+		return tipoVia;
+	}
+
+	public void setTipoVia(String tipoVia) {
+		this.tipoVia = tipoVia;
+	}
+
+	public String getVia() {
+		return via;
+	}
+
+	public void setVia(String via) {
+		this.via = via;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getPiso() {
+		return piso;
+	}
+
+	public void setPiso(int piso) {
+		this.piso = piso;
+	}
+
+	public String getPuerta() {
+		return puerta;
+	}
+
+	public void setPuerta(String puerta) {
+		this.puerta = puerta;
+	}
+
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	@Override
+	public String toString() {
+		return "Domicilio (" + tipoVia + ", " + via + ", " + numero + ", " + piso + ", " + puerta + ", " + codigoPostal
+				+ ", " + ciudad + ", " + provincia + ")";
+	}
+
+}
