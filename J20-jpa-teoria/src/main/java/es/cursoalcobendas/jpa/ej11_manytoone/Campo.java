6dea8530b410467a810b510f24132a6274ac1287
pablo <cursos.tic.plg@gmail.com>
2026-07-21 15:50:04 +0200

    act

---
+++ J20-jpa-teoria/src/main/java/es/cursoalcobendas/jpa/ej11_manytoone/Campo.java
@@ -0,0 +1,12 @@
+package es.cursoalcobendas.jpa.ej11_manytoone;
+
+import java.io.Serializable;
+
+public class Campo implements Serializable {
+
+	private Integer idCampo;
+	private String campo;
+	private String direccion;
+	
+	
+}
